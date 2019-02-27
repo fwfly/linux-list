cc4e06fa60f842175a8a82ca4a104837f81a9624
Wang Sheng Wen <wangshengwen@Wangde-MacBook-Air.local>
2019-02-27 18:14:09 +1100

    Create a list and clean it after sorting

--- examples/merge-sort.c
+++ examples/merge-sort.c
@@ -1,11 +1,42 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <assert.h>
-#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include "common.h"
+#include "list.h"
+
+static uint16_t values[256];
 
 int main(void)
 {
     printf("merge-sort\n");
+    struct list_head testlist;
+    struct listitem *item, *is = NULL;
+    size_t i;
+
+    random_shuffle_array(values, (uint16_t) ARRAY_SIZE(values));
+
+    INIT_LIST_HEAD(&testlist);
+
+    assert(list_empty(&testlist));
+
+    /* Add values into array */
+    for (i = 0; i < ARRAY_SIZE(values); i++) {
+        item = (struct listitem *) malloc(sizeof(*item));
+        item->i = values[i];
+        list_add_tail(&item->list, &testlist);
+    }
+
+    /* merge sort */
+
+    /* clean list */
+    i = 0;
+    list_for_each_entry_safe (item, is, &testlist, list) {
+        list_del(&item->list);
+        free(item);
+        i++;
+    }
+
+    assert(i == ARRAY_SIZE(values));
+    assert(list_empty(&testlist));
     return 0;
 }
