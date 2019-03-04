efc94b6e5326fbf504134734012698b4ec08ae69
fwfly <fawafwfly@gmail.com>
2019-03-04 22:32:06 +1100

    Support divide list
    Divide a list into 2 list based on number of items

--- examples/merge-sort.c
+++ examples/merge-sort.c
@@ -4,7 +4,66 @@
 #include "common.h"
 #include "list.h"
 
-static uint16_t values[256];
+// origin is 256
+static uint16_t values[4];
+
+void devide_list(struct list_head *head,
+                 struct list_head *left_h,
+                 struct list_head *right_h,
+                 uint16_t n)
+{
+    struct list_head *cur = head->next, *next = head->next->next;
+    size_t i = 0;
+    for (i = 0; i < n; i++) {
+        list_move_tail(cur, left_h);
+        cur = next;
+        next = next->next;
+    }
+    struct listitem *item;
+    item = list_entry(cur, struct listitem, list);
+
+    printf("devide %d \n", item->i);
+    list_splice_tail(head, right_h);
+
+}
+/*void sort_left_right( struct list_head *list_l, struct list_head *list_r,
+uint16_t n)
+{
+  printf("sort_left_right\n");
+}*/
+
+void print_list(struct list_head *head)
+{
+    printf("print_list\n");
+    struct listitem *item;
+    list_for_each_entry (item, head, list) {
+        printf("%d ", item->i);
+    }
+    printf("\n");
+}
+
+void merge_sort(struct list_head *head, uint16_t n)
+{
+    struct list_head list_l, list_r;
+    INIT_LIST_HEAD(&list_l);
+    INIT_LIST_HEAD(&list_r);
+
+
+    if (n < 2)
+        return;
+
+    // split into 2
+    n = n / 2;
+    devide_list(head, &list_l, &list_r, n);
+    print_list(&list_l);
+    printf("finish left\n");
+    print_list(&list_r);
+    printf("finish right\n");
+    // merge_sort(list_l, n);
+    // merge_sort(list_r, n);
+    // head = sort_left_right(list_l, list_r);
+
+}
 
 int main(void)
 {
@@ -27,6 +86,8 @@ int main(void)
     }
 
     /* merge sort */
+    print_list(&testlist);
+    merge_sort(&testlist, ARRAY_SIZE(values));
 
     /* clean list */
     i = 0;
